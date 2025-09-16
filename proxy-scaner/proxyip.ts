5d817b672caebddc657943985e5f25463c71baa5
exball <136123735+exball@users.noreply.github.com>
2025-09-16 12:56:27 +0800

    Update proxyip.ts

--- proxy-scaner/proxyip.ts
+++ proxy-scaner/proxyip.ts
@@ -30,8 +30,8 @@ async function getNextTokenIndex(): Promise<number> {
 import * as tls from "tls";
 
 // Timeout configuration (adjust here)
-export const FAST_TIMEOUT_MS = 2000;   // First pass timeout
-export const RETRY_TIMEOUT_MS = 5000;  // Retry pass timeout
+export const FAST_TIMEOUT_MS = 3000;   // First pass timeout
+export const RETRY_TIMEOUT_MS = 1000;  // Retry pass timeout
 
 interface ProxyStruct {
   address: string;
